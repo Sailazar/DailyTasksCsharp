30a3376b3f987c43ec234eef2479ba41ee9c02d5
Sailazar <rafailov2006@gmail.com>
2023-04-15 12:17:48 +0300

    Second exercise

--- RandomNumbers.cs
+++ RandomNumbers.cs
@@ -7,10 +7,53 @@ int[] GetRandomNumbers()
     return array;
 }
 
+//Printing the numbers
 int[] randomNumbers = GetRandomNumbers();
-Console.WriteLine("Random numbers: ");
-foreach (int number in randomNumbers)
+Console.Write("Random numbers: ");
+foreach(int number in randomNumbers)
 {
     Console.Write(number + " ");
 }
 Console.WriteLine("");
+
+//Sum all the numbers
+
+Console.Write("Sum of all the numbers: ");
+int sum = 0;
+foreach(int number in randomNumbers)
+{
+    sum += number;
+}
+Console.WriteLine(sum);
+
+
+//Put all the numbers that are less than 2 in a separate list 
+
+Console.Write("List with numbers < 2:  ");
+List<int> list = new();
+foreach(int number in randomNumbers)
+{
+    if (number < 2)
+    {
+        list.Add(number);
+    }
+}
+
+int j = 0;
+
+while (j < list.Count) 
+{
+    Console.Write(list[j] + " ");
+    ++j;
+} 
+Console.WriteLine();
+
+//Sort the list in ascending order
+
+Console.Write("The list in ascending order: ");
+list.Sort();
+foreach(int number in list)
+{
+    Console.Write(number + " ");
+}
+Console.WriteLine();
